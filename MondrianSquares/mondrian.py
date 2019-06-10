92c3df6bd53382573d6e98b1137fb20b8b100696
Joseph Wang <wangj4@otpp.com>
2019-06-10 16:25:18 -0400

    Fixed time counter functions

--- MondrianSquares/mondrian.py
+++ MondrianSquares/mondrian.py
@@ -18,6 +18,11 @@ def defect(numset):
     return max(numset) - min(numset)
 
 
+def area(rect):
+    """Calculates the area of a rectangle."""
+    return rect[0] * rect[1]
+
+
 def optimal(size):
     """Returns a(n) for 3 <= n <= 57 where a(n) is the optimal tiling of the
     Mondrian Art Puzzle.
@@ -39,8 +44,8 @@ def create_rect_list(size, quiet=False):
         for b in range(a, size + 1):
             if a * b > ceiling:
                 break
-            rect_list = numpy.append(rect_list, [a * b, a, b])
-    rect_list = rect_list.reshape(-1, 3)
+            rect_list = numpy.append(rect_list, [a, b])
+    rect_list = rect_list.reshape(-1, 2)
     if not quiet:
         print("Rectangle list generation complete")
     return rect_list
@@ -84,11 +89,13 @@ def search_sum(size, bound=0, quiet=False):
         else:
             bound = size
     rect_list = create_rect_list(size, quiet)
-    size_list = rect_list[:, 0]  # View of rect_list
+    if not quiet:
+        print(rect_list)
     valid = set()
     lower = 0
     while True:
-        searchlist = [x for x in size_list if x >= size_list[-1] - bound]
+        searchlist = [x for x in rect_list
+                      if area(x) >= area(rect_list[-1]) - bound]
         # Check if range overlaps
         if searchlist[0] != lower:
             # Search start index
@@ -98,10 +105,9 @@ def search_sum(size, bound=0, quiet=False):
             if size_list[-1] <= bound + 1:
                 break
         lower = searchlist[0]
-        size_list = [x for x in size_list if x < size_list[-1]]
-    if not quiet:
-        print(rect_list)
-    return [rect_list, valid]
+        #size_list = [x for x in size_list if x < size_list[-1]]
+        rect_list = rect_list[:-1]
+    return valid
 # ========================================================================== #
 
 
@@ -115,17 +121,25 @@ def min_theoretical_defect(size):
     return tdefect
 
 
+def count_time(value, quiet=False):
+    """Returns the running times of start through stop inclusive."""
+    if not quiet:
+        print("Searching size", value)
+    timestart = time.perf_counter()
+    search_sum(value, quiet)
+    duration = round(time.perf_counter() - timestart, 3)
+    if not quiet:
+        print(f"Size {value} takes {duration} s.")
+    return duration
+
+
 def record_time(start=3, stop=10, quiet=False):
     """Records the running times of start through stop inclusive."""
     f = open('MondrianSquares/mondrian-time.json', 'r')
     timedict = dict(json.load(f))
     f.close()
     for a in range(start, stop + 1):
-        if not quiet:
-            print("Searching size", a)
-        timestart = time.perf_counter()
-        search_sum(a, quiet)
-        timedict[str(a)] = round(time.perf_counter() - timestart, 3)
+        timedict[str(a)] = count_time(a, quiet)
         print(timedict)
         f = open('MondrianSquares/mondrian-time.json', 'w')
         f.write(json.dumps(timedict))
@@ -133,4 +147,5 @@ def record_time(start=3, stop=10, quiet=False):
     if not quiet:
         print(f"Finished searching sizes {start} through {stop}")
 
-record_time(start=18, stop=18)
+#record_time(start=18, stop=18)
+search_sum(5)
