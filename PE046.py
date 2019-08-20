53f6fbee51c603405ed9011040e6765a16a12fd7
Joseph Wang <wangj4@otpp.com>
2019-08-20 12:04:05 -0400

    Improved code

--- PE046.py
+++ PE046.py
@@ -1,34 +1,19 @@
-from Miller_Primality import is_prime
+from _prime_tools import is_prime
+from math import sqrt
 
 
-def generate(prev):
-    prev += 2
-    while not is_prime(prev):
-        prev += 2
-    return prev
-
-
-def goldbach(candidate):
+def goldbach(vol=0):
+    pset = set([2, 3, 5, 7])
+    cand = 9
     while True:
-        test = False
-        index = 0
-        while candidate > primes[index]:
-            square = 1
-            while candidate > primes[index] + 2 * square**2:
-                square += 1
-            if candidate == primes[index] + 2 * square**2:
-                test = True
-                break
-            index += 1
-        if test:
-            if vol >= 3:
-                print("%d failed" % candidate)
-            candidate = generate(candidate)
-        else:
-            if vol >= 1:
-                print("%d is a counterexample!" % candidate)
-            break
+        while is_prime(cand):
+            pset.add(cand)
+            cand += 2
+        cset = set([cand - 2*x*x for x in range(1, int(sqrt(cand/2)) + 1)])
+        if cset & pset == set():
+            return cand
+        cand += 2
 
 
 def solve(vol=0):
-    return goldbach(9, vol=vol)
+    return goldbach()
