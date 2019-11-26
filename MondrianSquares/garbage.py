0b0a7e900c85537079ef3698f94e972f12b114cb
Joseph Wang <joseph4x4@gmail.com>
2019-11-26 17:12:47 -0500

    Moved to new folder

--- MondrianSquares/garbage.py
+++
@@ -1,6 +0,0 @@
-import time
-
-t = time.time() - 1561263979.8564188
-print(time.time())
-print(time.strftime('%j,%H:%M:%S', time.gmtime(t)))
-print(4//4)
--- PE044.py
+++ PE044.py
@@ -1,4 +1,42 @@
-def min_diff(vol=0):
+from math import sqrt
+from itertools import count
+
+
+def pent(n):
+    """ Calculates the nth pentagonal number. """
+    return int(n * (3 * n - 1) / 2)
+
+
+def is_pent(p):
+    """ Predicate for whether p is pentagonal. """
+    if p <= 0:
+        return False
+    n = int(sqrt(p * 2 / 3)) + 1
+    while pent(n) > p:
+        n -= 1
+    return pent(n) == p
+
+
+def min_diff(vol=0, prove=False):
+    """
+    Calculates the minimum difference of any pair of pentagonal numbers whose
+    sum and differences are also pentagonal.
+    """
+    d = 0
+    for n1 in count(2):
+        if d and pent(n1) - pent(n1 - 1) > d:
+            return d
+        for n2 in range(1, n1):
+            if d and pent(n1) - pent(n2) > d:  # Only runs if prove is True
+                continue
+            if is_pent(pent(n1) + pent(n2)) and is_pent(pent(n1) - pent(n2)):
+                if not d or pent(n1) - pent(n2) < d:
+                    d = pent(n1) - pent(n2)
+                    if not prove:
+                        return d
+            
+
+def min_diff_old(vol=0):
     """
     Calculates the minimum difference of any pair of pentagonal numbers whose
     sum and differences are also pentagonal.
@@ -28,3 +66,5 @@ def min_diff(vol=0):
 
 def solve(vol=0):
     return min_diff(vol=vol)
+
+solve()
